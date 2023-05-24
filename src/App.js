24a1145c82f36464f95ce1d48e5dc2ad39dda8b5
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-05-24 21:08:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import "./scss/index.scss";
 
 function App() {
   return (
-    <div>App</div>
+    <div className='dashboard'></div>
   )
 }
 
